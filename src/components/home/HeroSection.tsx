cc382188253520773c25bb0d97ddccb1e624e71a
beniskillen <ben_killen@hotmail.com>
2026-02-18 14:27:40 +0800

    Delete Hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,7 +1,6 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
-import heroVideo from 'https://www.youtube.com/watch?v=hlbbFR9j9PY';
 
 export const HeroSection = () => {
   return (
