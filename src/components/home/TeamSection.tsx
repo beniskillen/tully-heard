6d3742eb1b4accb900a392c116ae5293212cf90d
beniskillen <ben_killen@hotmail.com>
2026-02-20 20:26:22 +0800

    jtheadshot test

--- src/components/home/TeamSection.tsx
+++ src/components/home/TeamSection.tsx
@@ -8,7 +8,7 @@ const teamMembers = [
     name: 'John Tully',
     role: 'Managing Director',
     specialty: 'Strategic Planning & Venue Development',
-    image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop&crop=faces',
+    image: 'jt-headshot.jpg',
   },
   {
     name: 'Luke Heard',
